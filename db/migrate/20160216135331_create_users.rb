b2982b1af796cbab2d958039118d3259496aa198
CDJimmy <charrierdjimmy@gmail.com>
2016-02-27 16:26:03 +0100

    correct snake_case column

--- db/migrate/20160216135331_create_users.rb
+++ db/migrate/20160216135331_create_users.rb
@@ -2,14 +2,14 @@ class CreateUsers < ActiveRecord::Migration
   def up
     create_table :users do |t|
       t.index   :id
-      t.integer :indexOrder
-      t.string  :firstName
-      t.string  :lastName
+      t.integer :index_order
+      t.string  :first_name
+      t.string  :last_name
       t.string  :civility
       t.integer :age
       t.string  :email
-      t.string  :colorBackground
-      t.string  :colorTitle
+      t.string  :color_background
+      t.string  :color_title
 
       t.timestamps null: false
     end
